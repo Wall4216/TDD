04cedd61a2de6a8b67edf37821ec2850f7feea61
Wall <ilnazamirhanov61@gmail.com>
2023-03-20 00:00:02 +0300

    add backend test

--- laravel/app/Http/Controllers/PostController.php
+++ laravel/app/Http/Controllers/PostController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Post\StoreRequest;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function store()
+    public function store(StoreRequest $request)
     {
-
+        $data = $request->validated();
+        Post::create($data);
     }
 }
